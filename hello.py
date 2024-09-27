8131db977862e98ac43c8634dbef78af28091a8b
Valentina C. Prado <85965282+valencprado@users.noreply.github.com>
2024-09-26 21:36:00 -0300

    feat: nova rota

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 # A very simple Flask Hello World app for you to get started with...
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/')
 def hello_world():
     return '<p>Hello from Flask!</p><table><tr><td><b>Aluno:</b></td><td>Valentina Corradini Prado</td></tr><tr><td><b>Prontuário:</b></td><td>PT302539X</td></tr></table>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
