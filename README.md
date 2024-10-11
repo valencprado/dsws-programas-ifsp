3f8b6c4ed9733727b0c48dc61e7fbb53a75362e6
Valentina <prado.valentina@aluno.ifsp>
2024-10-10 21:24:51 -0300

    código da aula

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Desenvolvimento Web Servidor: API's com Flask
 Códigos da matéria de "Desenvolvimento Web: Servidor" do Instituto Federal de São Paulo - Campus Pirituba
+
+## Libs instaladas
+- flask-bootstrap
+- flask-moment
--- hello.py
+++ hello.py
@@ -1,16 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def hello_world():
     return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Fabio%20Teixeira/PT23820X/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
 
-@app.route('/user/<name>/<register>/<institution>')
-def user(name, register, institution):
-    return '<h1>Avaliação Contínua: Aula 030</h1><h1>Aluno: {n}</h1><h1>Prontuário: {r}</h1><h1>Instituição: {i}</h1> <a href="/">Voltar</a>'.format(n = name, r=register, i=institution)
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 
 @app.route('/contextorequisicao')
