985abf1444a27695d5f0e18e95047d0600631bf4
Valentina Prado <valenpschool@gmail.com>
2024-11-01 13:53:37 -0300

    [AV05] código da avaliação

--- forms.py
+++ forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class NameForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Informe o seu nome:', validators=[DataRequired()])
+    last_name = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
+    institution = StringField('Informe a sua instituição de ensino:', validators=[DataRequired()])
+    subject = SelectField('Informe a sua disciplina:', choices=["DSWA5", "DWBA4", "Gestão de Projetos"], validators=[DataRequired()])
     submit = SubmitField('Submit')
--- hello.py
+++ hello.py
@@ -1,44 +1,41 @@
+from __future__ import print_function
 from flask import Flask, request, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 # from datetime import datetime, UTC
 from datetime import datetime
 from forms import NameForm
+import sys
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'oRcm36otmAKZ9xLxJokVoCj312RH1qIp1kJEOMzku2CVmDwADQMf5Wu9KIlaCMXE'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
    now = datetime.utcnow()
 #    now = datetime.now(UTC)
-   return render_template('index.html', current_time=now)
+   form = NameForm()
+   ip = None
+   host = None
+  
+   if form.validate_on_submit():
+       print(form.subject.data, file=sys.stderr)
+       old_name = session.get('name')
+       if old_name is not None and old_name != form.name.data:
+           flash('Você alterou o seu nome!')
+       session['name'] = form.name.data
+       session['institution'] = form.institution.data
+       session['subject'] = form.subject.data
+
+       form.name.data = ''
+       ip = request.remote_addr
+       host = request.host
+    #    return redirect(url_for('index'))
+   return render_template('index.html', current_time=now, form=form, name=session.get('name'), ip=ip, host=host, institution=session.get('institution'), subject=session.get('subject'))
 
 
-@app.route('/user/<name>/<register>/<institution>')
-def user(name, register, institution):
-    return render_template('user.html', name=session.get('name'), register=register, institution=institution)
-
-@app.route('/contextorequisicao/<name>')
-def contexto_requisicao(name):
-    browser = request.headers.get('User-Agent')
-    ip = request.remote_addr
-    host = request.host
-    return render_template('contexto_requisicao.html',name=name, browser=browser, ip=ip, host=host)
-
-@app.route('/form', methods=['GET', 'POST'])
-def form():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Novo nome! Bonito, hein?')
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('form'))
-    return render_template('form.html', form=form, name=session.get('name'))
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
