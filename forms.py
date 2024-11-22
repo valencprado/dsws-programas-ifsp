d4e1913f565511d68463c1ffe4b9ec0c29ada80f
Valentina Prado <valenpschool@gmail.com>
2024-11-21 22:53:22 -0300

    [AV09] nova tabela e formulário

--- forms.py
+++ forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
-class NameForm(FlaskForm):
+class UserForm(FlaskForm):
     name = StringField('Informe o seu nome:', validators=[DataRequired()])
+    role = SelectField('Role?:', choices=[])
     submit = SubmitField('Submit')
+
+    def populate_choices(self):
+        from hello import Role
+        self.role.choices = [(role.id, role.name) for role in Role.query.all()]
--- hello.py
+++ hello.py
@@ -2,9 +2,7 @@ import os
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from forms import UserForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -41,12 +39,6 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
@@ -64,19 +56,23 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = UserForm()
+    form.populate_choices()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user_role =  Role.query.filter_by(name='User').first()
-            user = User(username=form.name.data, role=user_role)
+            user = User(username=form.name.data, role_id=form.role.data)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
         else:
             session['known'] = True
         session['name'] = form.name.data
+        session['role'] = form.role.data
         return redirect(url_for('index'))
     users = User.query.all()    
+    roles = Role.query.all()
+    users_count = User.query.count()
+    roles_count = Role.query.count()
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False), users=users)
+                           known=session.get('known', False), users=users, users_count=users_count, roles=roles, roles_count=roles_count)
