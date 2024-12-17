af0f325473dcc769488f4bce47cfc14c0a19d2f9
Valentina C. Prado <85965282+valencprado@users.noreply.github.com>
2024-12-17 19:28:25 -0300

    [AV12] variável de ambiente

--- create_environment_variables.sh
+++ create_environment_variables.sh
@@ -1,6 +1,6 @@
 export FLASK_APP=flasky.py
 export FLASK_DEBUG=1
-export MAIL_USERNAME=flaskaulas@zohomail.com
+export MAIL_USERNAME=valenpschool@zohomail.com
 export MAIL_PASSWORD=$1
-export FLASKY_ADMIN=fabio.teixeira@ifsp.edu.br
+export FLASKY_ADMIN=prado.valentina@ifsp.edu.br
 export FLASKY_CONFIG=DevelopmentConfig
