c4cb0044646f9ca25c1a7a32009d617910ede100
Valentina C. Prado <85965282+valencprado@users.noreply.github.com>
2024-12-17 20:01:22 -0300

    [AV12] lógica de email

--- app/email.py
+++ app/email.py
@@ -1,22 +1,40 @@
+from threading import Thread
+from flask import current_app, render_template
+from flask_mail import Message
+from . import mail
 
-from flask import current_app
 import requests
-import datetime
+from datetime import datetime
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])    
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 def send_simple_message(to, subject, newUser):
+    app = current_app._get_current_object()
     print('Enviando mensagem (POST)...', flush=True)
-    print('URL: ' + str(current_app.config['API_URL']), flush=True)
-    print('api: ' + str(current_app.config['API_KEY']), flush=True)
-    print('from: ' + str(current_app.config['API_FROM']), flush=True)
+    print('URL: ' + str(app.config['API_URL']), flush=True)
+    print('api: ' + str(app.config['API_KEY']), flush=True)
+    print('from: ' + str(app.config['API_FROM']), flush=True)
     print('to: ' + str(to), flush=True)
-    print('subject: ' + str(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, flush=True)
+    print('subject: ' + str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, flush=True)
     print('text: ' + "Novo usuário cadastrado: " + newUser, flush=True)
 
-    resposta = requests.post(current_app.config['API_URL'],
-                             auth=("api", current_app.config['API_KEY']), data={"from": app.config['API_FROM'],
-                                                                        "to": to,
-                                                                        "subject": app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                                                                        "text": f"Novo usuário cadastrado: {newUser}\nAluno: Valentina Corradini Prado\n Prontuário: PT302539X"})
-
+    resposta = requests.post(app.config['API_URL'], 
+                             auth=("api", app.config['API_KEY']), data={"from": app.config['API_FROM'], 
+                                                                        "to": to, 
+                                                                        "subject": app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, 
+                                                                        "text": "Novo usuário cadastrado: " + newUser})
+        
     print('Enviando mensagem (Resposta)...' + str(resposta) + ' - ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), flush=True)
     return resposta
