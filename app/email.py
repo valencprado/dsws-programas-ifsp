4f10b81462108c6eb2493c6fadeb89f8f44ef01d
Valentina C. Prado <85965282+valencprado@users.noreply.github.com>
2024-12-17 19:50:32 -0300

    [AV12] correção

--- app/email.py
+++ app/email.py
@@ -1,19 +1,19 @@
 
-from .. import app
+from flask import current_app
 import requests
 import datetime
 
 def send_simple_message(to, subject, newUser):
     print('Enviando mensagem (POST)...', flush=True)
-    print('URL: ' + str(app.config['API_URL']), flush=True)
-    print('api: ' + str(app.config['API_KEY']), flush=True)
-    print('from: ' + str(app.config['API_FROM']), flush=True)
+    print('URL: ' + str(current_app.config['API_URL']), flush=True)
+    print('api: ' + str(current_app.config['API_KEY']), flush=True)
+    print('from: ' + str(current_app.config['API_FROM']), flush=True)
     print('to: ' + str(to), flush=True)
-    print('subject: ' + str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, flush=True)
+    print('subject: ' + str(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, flush=True)
     print('text: ' + "Novo usuário cadastrado: " + newUser, flush=True)
 
-    resposta = requests.post(app.config['API_URL'],
-                             auth=("api", app.config['API_KEY']), data={"from": app.config['API_FROM'],
+    resposta = requests.post(current_app.config['API_URL'],
+                             auth=("api", current_app.config['API_KEY']), data={"from": app.config['API_FROM'],
                                                                         "to": to,
                                                                         "subject": app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                                                                         "text": f"Novo usuário cadastrado: {newUser}\nAluno: Valentina Corradini Prado\n Prontuário: PT302539X"})
