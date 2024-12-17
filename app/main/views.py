0134820d37a0427555ff75f7ff25aeb45eb9608e
Valentina C. Prado <85965282+valencprado@users.noreply.github.com>
2024-12-17 19:48:52 -0300

    [AV12] correções

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 
 
 from flask import redirect, render_template, url_for, session, current_app
-from .. import db, app
+from .. import db
 from ..models import User, Role
-from ..email import User, Role, send_simple_message
+from ..email import  send_simple_message
 from .forms import NameForm
 from . import main
 
--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'flaskaulas@zohomail.com'
+    FLASKY_MAIL_SENDER = 'valenpschool@gmail.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
